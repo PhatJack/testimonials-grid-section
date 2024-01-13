31ebc058287379ae899ccdd32d70f465f60deb66
PhatJack <tienphatng.693@gmail.com>
2024-01-13 21:10:03 +0700

    final solution

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./*.{html,js}"],
+	theme: {
+		extend: {
+			colors: {
+				"moderate-violet": "hsl(263, 55%, 52%)",
+				"very-dark-grayish-blue": "hsl(217, 19%, 35%)",
+				"very-dark-blackish-blue": "hsl(219, 29%, 14%)",
+				"white": "hsl(0, 0%, 100%)",
+				"light-gray": "hsl(0, 0%, 81%)",
+				"light-grayish-blue": "hsl(210, 46%, 95%)"
+			},
+			fontFamily: {
+				"barlow" : "Barlow",
+				"barlow-medium" : "Barlow-Medium",
+				"barlow-semibold" : "Barlow-Semibold",
+			}
+		},
+	},
+	plugins: [],
+}
+
